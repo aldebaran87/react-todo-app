5bd29acbcf9cc6d357c9fe3fd1e8164ddcd8f2a4
Avoda <aldebaran87@i.ua>
2023-01-20 15:25:53 +0200

    second commit

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -10,7 +10,7 @@ const Todoitem = ({ text, deleteH, toggleTodo }) => {
             <RiTodoFill className={styles.icon_task} />
             <p>{text.text}</p>
             <RiDeleteBin2Line onClick={() => deleteH(text.id)} className={styles.deleteIcon} />
-            <FaCheck className={styles.checkIcon} onClick={() => toggleTodo(text.id)} />
+            <FaCheck className={` ${!text.isComplited ? styles.checkIcon : styles.cheked_checkIcon}`} onClick={() => toggleTodo(text.id)} />
         </div>
     )
 }
